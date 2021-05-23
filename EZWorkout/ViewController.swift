d167850856af1c205062c9170b928f14fe220f1b
Daniel <ramirezdaniel51@yahoo.com>
2021-05-23 12:04:27 -0700

    Initial Commit

---
+++ EZWorkout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EZWorkout
+//
+//  Created by Daniel Ramirez on 5/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
